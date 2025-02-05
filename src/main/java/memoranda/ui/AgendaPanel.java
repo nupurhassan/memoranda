9a3dba8f4eae351d6b4c55cce0a6753eb53436a6
nupurhassan <nupur.hassan3@gmail.com>
2025-02-04 23:35:55 -0700

    English translation in AgendaPanel on Import

--- src/main/java/memoranda/ui/AgendaPanel.java
+++ src/main/java/memoranda/ui/AgendaPanel.java
@@ -205,7 +205,7 @@ public class AgendaPanel extends JPanel {
 						 //JOptionPane.showMessageDialog(null,name);
 					}else if (d.startsWith("memoranda:importstickers")) {
 						final JFrame parent = new JFrame();
-						String name = JOptionPane.showInputDialog(parent,Local.getString("Ingrese nombre de archivo a importar"),null);
+						String name = JOptionPane.showInputDialog(parent,Local.getString("Enter filename to Import"),null);
 						new ImportSticker(name).import_file();
 					}
 				}
